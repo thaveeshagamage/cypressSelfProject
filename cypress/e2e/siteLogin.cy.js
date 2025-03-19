9af2f600779acf5a1dbe4ff01ab36697ba886087
Thaveesha Gamage <gamage.thaveesha@gmail.com>
2025-03-19 13:06:25 +0530

    merge cypress files

---
+++ cypress/e2e/siteLogin.cy.js
@@ -0,0 +1,9 @@
+describe('Login to site', () => {
+
+    it('Navigate to page', () => {
+      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+      
+    })
+
+
+  })
